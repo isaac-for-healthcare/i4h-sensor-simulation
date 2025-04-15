1fb311523b5c5cb3e617f014e8dfd1494a25b28d
Yiheng Wang <vennw@nvidia.com>
2025-04-15 03:39:53 +0000

    fix pre commit
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ultrasound-raytracing/cmake/SetupCUDA.cmake
+++ ultrasound-raytracing/cmake/SetupCUDA.cmake
@@ -85,7 +85,7 @@ endfunction()
 # Start with "70", this is the lowest architecture supported by cuFFTDx
 if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
     message(STATUS "CMAKE_CUDA_ARCHITECTURES not defined, setting it to `native`")
-    set(CMAKE_CUDA_ARCHITECTURES native)  
+    set(CMAKE_CUDA_ARCHITECTURES native)
 endif()
 if(CMAKE_CUDA_ARCHITECTURES STREQUAL "all")
     set(CMAKE_CUDA_ARCHITECTURES "70;72;75;80;86;87;89;90;90a")
