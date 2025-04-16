5e3a8a0405e95460a60e568f3c7fc90474d3e774
Yiheng Wang <vennw@nvidia.com>
2025-04-16 13:29:08 +0800

    update download script
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- ultrasound-raytracing/examples/raytracing_to_us/us_dataset/download_data.sh
+++ ultrasound-raytracing/examples/raytracing_to_us/us_dataset/download_data.sh
@@ -1,8 +1,8 @@
 wget https://zenodo.org/api/records/7272660/files-archive
 mv files-archive files.zip
-mkdir ~/zenodo_dataset
-unzip files.zip -d ~/zenodo_dataset
-cd ~/zenodo_dataset
+mkdir zenodo_dataset
+unzip files.zip -d zenodo_dataset
+cd zenodo_dataset
 unzip Benign.zip
 unzip Malignant.zip
 unzip Normal.zip
